35bbdd85eef5cc475caaf14d57323778482d7f9f
TshepoMatshisevhe <matshisevhe.tshepo@gmail.com>
2024-09-27 14:55:24 +0200

    Initial commit for HelloWorldApp

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorldApp{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Welcome to my first C# project!");
+            
+            Console.Write("What's your name? ");
+            string? name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}! Hope you're having a great day!");
+        }
+    }
+}
